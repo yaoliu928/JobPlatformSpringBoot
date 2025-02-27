87451f2cc083782c80fc5d21ac520bb4b77acef1
Angela <yaoliu928@gmail.com>
2025-02-27 22:23:03 +1000

    initial sb project

---
+++ src/main/java/com/mycompany/jobspringboot/JobSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.jobspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/jobspringboot/JobSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.jobspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
