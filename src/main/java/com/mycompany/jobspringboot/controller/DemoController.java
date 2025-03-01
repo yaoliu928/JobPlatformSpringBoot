fb3a500ad3831d50d12d2f0a64fb7b5bf3c5a502
Angela <yaoliu928@gmail.com>
2025-03-01 22:32:13 +1000

    adding a post

--- src/main/java/com/mycompany/jobspringboot/controller/DemoController.java
+++ src/main/java/com/mycompany/jobspringboot/controller/DemoController.java
@@ -1,17 +1,25 @@
 package com.mycompany.jobspringboot.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RestController // @Controller and @ResponseBody
 public class DemoController {
     // handle get request
     @GetMapping("/get/userinfo")
     // make method return value to be the response
     @ResponseBody
-    public String getUseInfo(@RequestParam(value="nickname",required = true) String nickname) {
-        return "Hello " + nickname + "!";
+    public String getUseInfo(
+            @RequestParam(value="nickname",required = true) String nickname,
+            @RequestParam(value="age") String age
+    ) {
+        return "Hello " + nickname + ", you are " + age;
+    }
+
+    @PostMapping("/login")
+    public String login(){
+
+        return "login success";
     }
 }
