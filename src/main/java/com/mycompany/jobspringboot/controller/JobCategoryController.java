e04236975f9877b0c91ddb49696916ad3d7c9a32
Angela <yaoliu928@gmail.com>
2025-03-08 15:03:33 +1000

    develop controller layer

--- src/main/java/com/mycompany/jobspringboot/controller/JobCategoryController.java
+++ src/main/java/com/mycompany/jobspringboot/controller/JobCategoryController.java
@@ -1,4 +1,38 @@
 package com.mycompany.jobspringboot.controller;
 
+import com.mycompany.jobspringboot.domain.JobCategory;
+import com.mycompany.jobspringboot.service.JobCategoryService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class JobCategoryController {
+
+    @Resource
+    private JobCategoryService jobCategoryService;
+
+    @GetMapping("/all/category")
+    public List<JobCategory> getAllCategory(){
+        return jobCategoryService.getAllJobCategory();
+    }
+
+    @PostMapping("/add/category")
+    public int addCategory(JobCategory jobCategory){
+        return jobCategoryService.addJobCategory(jobCategory);
+    }
+
+    @PostMapping("/delete/category")
+    public int deleteCategory(@RequestParam(value = "id") Integer id){
+        return jobCategoryService.removeJobCategory(id);
+    }
+
+    @PostMapping("/update/category")
+    public int updateCategory(JobCategory jobCategory){
+        return jobCategoryService.updateJobCategory(jobCategory);
+    }
 }
